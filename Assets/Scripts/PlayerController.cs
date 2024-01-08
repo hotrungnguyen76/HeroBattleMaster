315612f80bd411e8da8071145b85301a710c6cb2
trungnguyen <hotrungnguyen168@gmail.com>
2024-01-09 05:35:38 +0700

    TileSet & player Jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,13 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirections))]
 public class PlayerController : MonoBehaviour
 {
+
     public float walkSpeed = 5f;
+
+    public float WalkSpeed
+    {
+        get {
+            if (touchingDirections.IsOnWall) {
+                return 0;
+            }
+            return walkSpeed;
+        }
+        set
+        {
+            walkSpeed = value;
+        }
+    } 
+
+    public float jumpImpulse = 8f;
+
     Vector2 moveInput;
 
     private bool _isMoving = false;
+
 
     public bool isMoving
     {
@@ -17,14 +36,17 @@ public class PlayerController : MonoBehaviour
         private set
         {
             _isMoving = value;
-            animator.SetBool("isMoving", value);
+            animator.SetBool(AnimationStrings.isMoving, value);
         }
     }
 
     Rigidbody2D rb2d;
     Animator animator;
+    TouchingDirections touchingDirections;
 
     private bool _isFacingRight = true;
+    
+
     public bool IsFacingRight
     {
         get => _isFacingRight;
@@ -43,23 +65,14 @@ public class PlayerController : MonoBehaviour
     {
         rb2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        touchingDirections = GetComponent<TouchingDirections>();
     }
 
     private void FixedUpdate()
     {
-        rb2d.velocity = new Vector2(moveInput.x * walkSpeed * Time.fixedDeltaTime, rb2d.velocity.y);   
+        
+        rb2d.velocity = new Vector2(moveInput.x * WalkSpeed * Time.fixedDeltaTime, rb2d.velocity.y);
+        
     }
 
     public void onMove(InputAction.CallbackContext context)
@@ -68,6 +81,18 @@ public class PlayerController : MonoBehaviour
         isMoving = moveInput != Vector2.zero;
         SetFacingDirection(moveInput);
     }
+
+    public void onJump(InputAction.CallbackContext context)
+    {
+        // To do: Check if alive 
+        if (context.started && touchingDirections.IsGrounded)
+        {
+            animator.SetTrigger(AnimationStrings.jump);
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpImpulse);
+        }
+    }
+
+
 
     private void SetFacingDirection(Vector2 moveInput)
     {
